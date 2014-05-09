77115f07b8fcff6768386f9328a87ec915489c05
Brunopp <marduk.dk@gmail.com>
2014-05-09 10:18:01 +0200

    Added Test projekt, refactored a method

---
+++ HusInfo/Controller/HouseCtr.cs
@@ -0,0 +1,54 @@
+﻿using HusInfo.Database;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace HusInfo.Controller
+{
+    public class HouseCtr
+    {
+		houseDatabaseDataContext db = new houseDatabaseDataContext();
+
+        public House GetHouse(int id)
+        {
+			var q = from h in db.Houses
+					where h.id == id
+					select h;
+
+            return q.FirstOrDefault();
+        }
+
+		public List<House> HouseSearch(string address)
+		{
+			var db = new houseDatabaseDataContext();
+			
+			var houses =
+				(from h in db.Houses
+				 where h.address.Contains(address)
+				 select h).Take(5).ToList();
+
+			return houses;
+		}
+
+		public List<Bitmap> getHousePictures(House house)
+		{
+			var q = from p in db.HousePics
+					where p.houseId == house.id
+					select getBitmap(p.picture.ToArray());
+
+			return q.ToList();
+		}
+
+		private Bitmap getBitmap(byte[] bytes)
+		{
+			ImageConverter ic = new ImageConverter();
+			Image img = (Image)ic.ConvertFrom(bytes);
+			Bitmap bmp = new Bitmap(img);
+
+			return bmp;
+		}
+    }
+}
---
+++ ProjektHusInfoTest/Controller/HouseCtrTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HusInfo.Controller;
+using HusInfo.Database;
+
+namespace ProjektHusInfoTest
+{
+    [TestClass]
+    public class HouseCtrTest
+    {
+        [TestMethod]
+        public void GetHouseTest()
+        {
+            HouseCtr hCtr = new HouseCtr();
+
+            House h = hCtr.GetHouse(3);
+
+			Assert.IsNotNull(h);
+        }
+    }
+}
