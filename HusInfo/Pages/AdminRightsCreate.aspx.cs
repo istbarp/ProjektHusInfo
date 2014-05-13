2678a5da884612b3d3ae24bc25274a523dae52be
Danny Melsen <dannymelsen@gmail.com>
2014-05-13 13:08:14 +0200

    AdminRights Edit - Create

---
+++ HusInfo/Pages/AdminRightsCreate.aspx.cs
@@ -0,0 +1,47 @@
+﻿using HusInfo.Controller;
+using HusInfo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HusInfo.Pages
+{
+    public partial class AdminRightsCreate : System.Web.UI.Page
+    {
+        HouseCtr hCtr = new HouseCtr();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonCreate_Click(object sender, EventArgs e)
+        {
+            House h = new House();
+            h.address = AddressTb.Text;
+            h.basementAreal = Int32.Parse(BasementArealTb.Text);
+            h.bruttoprice = Int32.Parse(BruttoPriceTb.Text);
+            h.buildingYear = Int32.Parse(BuildingYearTb.Text);
+            h.cashPrice = Int32.Parse(CashPriceTb.Text);
+            h.city = CityTb.Text;
+            h.distToSchool = Int32.Parse(DistToSchoolTb.Text);
+            h.distToShopping = Int32.Parse(DistToShppingTb.Text);
+            h.energyMark = EnergyMarkTb.Text;
+            h.floorLevels = Int32.Parse(FloorLevelsTb.Text);
+            h.garageKvm = Int32.Parse(GarageArealTb.Text);
+            h.groundAreal = Int32.Parse(GroundArealTb.Text);
+            h.kvmPrice = Int32.Parse(KvmPriceTb.Text);
+            h.livingAreal = Int32.Parse(LivingArealTb.Text);
+            h.nettoPrice = Int32.Parse(NettoPriceTb.Text);
+            h.rooms = Int32.Parse(RoomsTb.Text);
+            h.toilets = Int32.Parse(ToiletsTb.Text);
+            h.webLink = WebLinkTb.Text;
+            h.zipCode = Int32.Parse(ZipcodeTb.Text);
+
+            hCtr.insertHouse(h);
+
+        }
+    }
+}
---
+++ HusInfo/Pages/AdminRightsEdit.aspx.cs
@@ -0,0 +1,58 @@
+﻿using HusInfo.Controller;
+using HusInfo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HusInfo.Controller;
+
+namespace HusInfo.Pages
+{
+    public partial class AdminRightsEdit : System.Web.UI.Page
+    {
+        HouseCtr hC = new HouseCtr();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<House> ha = hC.GetAllHouses();
+            foreach (House h in ha)
+            {
+                ha.Add(h);
+            }
+            DropDownListEdit.DataSource = ha;
+            DropDownListEdit.DataBind();
+        }
+
+        protected void ButtonEdit_Click(object sender, EventArgs e)
+        {
+            House h = new House();
+            h.address = AddressTbe.Text;
+            h.basementAreal = Int32.Parse(BasementArealTbe.Text);
+            h.bruttoprice = Int32.Parse(BruttoPriceTbe.Text);
+            h.buildingYear = Int32.Parse(BuildingYearTbe.Text);
+            h.cashPrice = Int32.Parse(CashPriceTbe.Text);
+            h.city = CityTbe.Text;
+            h.distToSchool = Int32.Parse(DistToSchoolTbe.Text);
+            h.distToShopping = Int32.Parse(DistToShppingTbe.Text);
+            h.energyMark = EnergyMarkTbe.Text;
+            h.floorLevels = Int32.Parse(FloorLevelsTbe.Text);
+            h.garageKvm = Int32.Parse(GarageArealTbe.Text);
+            h.groundAreal = Int32.Parse(GroundArealTbe.Text);
+            h.kvmPrice = Int32.Parse(KvmPriceTbe.Text);
+            h.livingAreal = Int32.Parse(LivingArealTbe.Text);
+            h.nettoPrice = Int32.Parse(NettoPriceTbe.Text);
+            h.rooms = Int32.Parse(RoomsTbe.Text);
+            h.toilets = Int32.Parse(ToiletsTbe.Text);
+            h.webLink = WebLinkTbe.Text;
+            h.zipCode = Int32.Parse(ZipcodeTbe.Text);
+
+            hC.editHouse(h);
+
+        }
+
+        protected void DropDownListEdit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
