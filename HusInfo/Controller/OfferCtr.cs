4fd4c553f3a38abef2d12534f9e4aa8d7d97fc16
Jesper Højer Larsen <j.hoejer89@gmail.com>
2014-05-15 13:07:08 +0200

    Offer Test and Offer Ctr.

---
+++ HusInfo/Controller/OfferCtr.cs
@@ -0,0 +1,47 @@
+﻿using HusInfo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HusInfo.Controller
+{
+    public class OfferCtr
+    {
+        HouseInfoContext db = new HouseInfoContext();
+
+
+        public void addOffer(Offer o)
+        {
+            db.Offer.Add(o);
+            db.SaveChanges();
+        }
+
+        public Offer getOffer(int id)
+        {
+            Offer q = (from o in db.Offer
+                    where o.id == id
+                    select o).FirstOrDefault();
+            return q;
+        }
+        public List<Offer> getOffers(int classificationId)
+        {
+            var q = from o in db.Offer
+                    where o.classificationId == classificationId
+                    select o;
+            return q.ToList();
+        }
+
+        public void deleteOffer(int id)
+        {
+            var q = (from o in db.Offer
+                     where o.id == id
+                     select o).FirstOrDefault();
+            db.Offer.Remove(q);
+
+            db.SaveChanges();
+        }
+
+
+    }
+}
---
+++ ProjektHusInfoTest/Controller/OfferCtrTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HusInfo.Controller;
+using HusInfo.Model;
+using System.Collections.Generic;
+
+namespace ProjektHusInfoTest.Controller
+{
+    [TestClass]
+    public class OfferCtrTest
+    {
+        OfferCtr oCtr = new OfferCtr();
+        [TestMethod]
+        public void CheckOfferTest()
+        {
+            Offer o = new Offer();
+            o.loginId = 5;
+            o.classificationId = 2;
+            o.price = 5000;
+            o.comment = "Sikke noget lort";
+
+            oCtr.addOffer(o);
+            int id = 2;
+
+            List<Offer> oNew = oCtr.getOffers(id);
+
+            Assert.IsTrue(oNew.Count > 0);
+
+            int offerId = oNew[oNew.Count - 1].id;
+
+            oCtr.deleteOffer(offerId);
+
+            Offer oDeleted = oCtr.getOffer(offerId);
+
+            Assert.IsNull(oDeleted);
+
+            
+        }
+    }
+}
