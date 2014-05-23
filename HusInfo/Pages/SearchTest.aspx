4a86890b3c73427edbb858f46f1a3c3930c75a72
Jesper Højer Larsen <j.hoejer89@gmail.com>
2014-05-23 11:31:34 +0200

    Added testsite for testing async search method.

---
+++ HusInfo/Pages/SearchTest.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Content.Master" AutoEventWireup="true" CodeBehind="SearchTest.aspx.cs" Inherits="HusInfo.Pages.SearchTest" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContainerPlaceHolder" runat="server">
+
+
+	<div class="jumbotron">
+				  <div class="input-group">
+					  <asp:TextBox runat="server" ID="searchBox" OnTextChanged="Unnamed_TextChanged" type="text" CssClass="form-control" placeholder="Indtast adresse"></asp:TextBox>
+					  <span class="input-group-btn">
+						  <button class="btn btn-default" type="submit">Søg</button>
+					  </span>
+				  </div>
+		  <br />
+
+		 
+		  <div class="panel panel-primary">
+			  <div class="panel-heading"><h3 class="panel-title" style="text-align:left">Søge resultater</h3></div>
+			  <div class="panel-body" style="text-align:left;">
+				<asp:PlaceHolder ID="searchResults" runat="server"></asp:PlaceHolder>
+			  </div>
+		  </div>
+
+
+      </div>
+</asp:Content>
---
+++ HusInfo/Pages/SearchTest.aspx.cs
@@ -0,0 +1,50 @@
+﻿using HusInfo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace HusInfo.Pages
+{
+	public partial class SearchTest : System.Web.UI.Page
+	{
+		protected string searchStr;
+		protected HusInfo.Controller.HouseCtr hCtr;
+		protected List<HusInfo.Model.House> hList;
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			searchStr = Request.QueryString["search"];
+
+			if (!string.IsNullOrEmpty(searchStr))
+			{
+				hCtr = new HusInfo.Controller.HouseCtr();
+				hList = hCtr.GetAllHouses();
+			}
+		}
+
+		protected void Unnamed_TextChanged(object sender, EventArgs e)
+		{
+			searchStr = searchBox.Text;
+			var blabla = from ere in hList
+						 where ere.address.Contains(searchStr)
+						 select ere;
+
+			blabla.ToList();
+
+			foreach(House h in blabla)
+			{
+				HtmlGenericControl div = new HtmlGenericControl("p");
+				div.ID ="p";
+				div.TagName = "p";
+				div.InnerText = h.address.ToString() + h.zipCode.ToString() + h.city.ToString();
+
+				searchResults.Controls.Add(div);
+			}
+		}
+
+
+	}
+}
